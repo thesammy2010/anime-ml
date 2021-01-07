c89accb8946220414d2877740f7631cc3ee8c4b3
Leo <you@example.com>
2021-01-07 21:02:06 +0000

    updated API url for new parameters

--- anime_ml/api/request.py
+++ anime_ml/api/request.py
@@ -49,8 +49,9 @@ def get_anime(anime_id: int) -> Dict[str, Any]:
     url: str = (
         f"{BASE_URL}/anime/{anime_id}?fields=id,title,alternative_titles,start_date,end_date,synopsis,"
         "mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,"
-        "my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background"
-        ",related_anime,recommendations,studios,statistics"
+        "list_status{start_date,end_date,priority,num_times_rewatched,rewatch_value,tags,comments},num_episodes,"
+        "start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,"
+        "recommendations,studios,statistics"
     )
 
     r = requests.get(url=url, headers=HEADERS)
